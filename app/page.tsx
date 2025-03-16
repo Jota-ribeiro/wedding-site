87ad1123a1ee914df99941d87e204048e2751071
Henrique <hesr.ribeiro@gmail.com>
2025-03-16 15:49:46 -0300

    componentiza prom section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AboutSection from "@/components/sections/AboutSection";
 import LocaleSection from "@/components/sections/LocaleSection";
+import PromSection from "@/components/sections/PromSection";
 import TitleSection from "@/components/sections/TitleSection";
 import clsx from "clsx";
 import Image from "next/image";
@@ -13,23 +14,7 @@ export default function Home() {
       <TitleSection />
       <AboutSection />
       <LocaleSection />
-      <section className={clsx('w-full flex flex-col justify-center items-center', 'gap-12')}>
-        <p className={clsx('font-beau text-[#142601]', 'text-3xl md:text-8xl')}>A Cerimônia</p>
-        <div className={clsx('w-full px-5 md:px-44 flex flex-row justify-center', 'gap-10 md:gap-20')}>
-          <div className={clsx('flex flex-col gap-5 md:gap-8', 'w-3/12')}>
-            <p className={clsx('font-cinzel md:text-4xl text-[#1D1B20]')}>Traje</p>
-            <p className={clsx('text-[#49454F] text-sm md:text-base')}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-          </div>
-          <div className={clsx('flex flex-col gap-5 md:gap-8', 'w-3/12')}>
-            <p className={clsx('font-cinzel md:text-4xl text-[#1D1B20]')}>Clima</p>
-            <p className={clsx('text-[#49454F] text-sm md:text-base')}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-          </div>
-          <div className={clsx('flex flex-col gap-5 md:gap-8', 'w-3/12')}>
-            <p className={clsx('font-cinzel md:text-4xl text-[#1D1B20]')}>Festa</p>
-            <p className={clsx('text-[#49454F] text-sm md:text-base')}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-          </div>
-        </div>
-      </section>
+      <PromSection />
     </main>
   );
 }

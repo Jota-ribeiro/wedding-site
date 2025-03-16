3be02114404036d1808a01a7ac4c8aa1acc9a18f
Henrique <hesr.ribeiro@gmail.com>
2025-03-16 15:23:55 -0300

    componetiza locale seciton

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import AboutSection from "@/components/sections/AboutSection";
+import LocaleSection from "@/components/sections/LocaleSection";
 import TitleSection from "@/components/sections/TitleSection";
 import clsx from "clsx";
 import Image from "next/image";
-import iconLupa from '@/public/img/icon-lupa.svg'
-import iconCalender from '@/public/img/icon-calendar.svg'
+
 
 
 
@@ -12,25 +12,7 @@ export default function Home() {
     <main className={clsx('w-full flex flex-col items-center', 'px-3 md:px-6 py-7', 'gap-8 md:gap-20')}>
       <TitleSection />
       <AboutSection />
-      <section className={clsx('w-full flex flex-col items-center md:-mt-8', 'gap-2 md:gap-5', 'text-[#142601]')}>
-        <p className={clsx('font-beau', 'text-3xl md:text-8xl')}>Junte-se a nós no nosso casamento</p>
-        <p className={clsx('font-cinzel md:text-4xl')}>16 de Junho de 2025</p>
-        <p className={clsx('font-cinzel md:text-4xl')}>às 16h00</p>
-        <p className={clsx('font-cinzel md:text-4xl')}>Lake House - Pampulha/BH</p>
-
-        <div className={clsx('w-full md:w-4/12 px-3 gap-3 flex flex-row justify-center')}>
-          <div className={clsx('w-6/12 bg-white', 'py-3 flex flex-row justify-center items-center gap-2', 'rounded', 'cursor-pointer')}>
-            <Image src={iconLupa} alt="" />
-
-            <p className={clsx('text-[11px] md:text-xs font-semibold')}>Como Chegar</p>
-          </div>
-          <div className={clsx('w-6/12 bg-[#142601]', 'py-3 flex flex-row justify-center items-center gap-2', 'rounded', 'cursor-pointer')}>
-            <Image src={iconCalender} alt="" />
-
-            <p className={clsx('text-[11px] md:text-xs text-white font-semibold')}>Confirmar Presença</p>
-          </div>
-        </div>
-      </section>
+      <LocaleSection />
     </main>
   );
 }

08fc8df4d5ffeedfc9962402b3665f18f4b11c2e
Henrique <hesr.ribeiro@gmail.com>
2025-03-30 17:17:00 -0300

    componentiza a firstsection mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,47 +2,23 @@
 
 //--Imports
 import clsx from "clsx";
-import imgCasal from '@/public/img/img-casal-example.jpg'
-import luizele from '@/public/img/img-casal.png'
-import chevronDown from '@/public/img/chevron-down.svg'
+
 //--Components
 import Header from "@/components/sections_v2/Header";
-import Image from "next/image";
-import { useState } from "react";
+import FirstSection from "@/components/sections_v2/FirstSection";
 
-import { ArrowLeftFromLineIcon } from 'lucide-react'
+
 
 export default function Home() {
 
-  const [showAside, setShowAside] = useState(true)
 
   return (
 
     <main>
       <Header />
-      <aside className={clsx(
-        'transition-all ease-in-out duration-500 h-screen font-abhaya font-semibold text-Cinza_main_100 text-xl',
-        'overflow-hidden w-64',
-        !showAside && 'w-0 opacity-0 translate-x-[-100%]' // Encolher e sumir para a esquerda
-      )}>
-        <div className={clsx('h-full flex flex-col bg-white border-r shadow-sm')}>
-          <div className={clsx('h-auto flex flex-col bg-[#f2fcfc] py-6 px-8 gap-2')}>
-            <div onClick={() => setShowAside(false)} className="self-end cursor-pointer">
-              <ArrowLeftFromLineIcon />
-            </div>
-            <Image className="w-5/12" src={luizele} alt="" />
-            <p className="text-lg">Alessandra <span className="text-red-400">&</span> Luiz Lima</p>
-          </div>
-          <div className="w-auto h-full flex flex-col py-6 px-8 gap-8">
-            <p className="cursor-pointer">Home</p>
-            <p className="cursor-pointer">O Casal</p>
-            <p className="cursor-pointer">Cerimônia</p>
-            <p className="cursor-pointer">Presentes</p>
-            <p className="cursor-pointer">Recados</p>
-          </div>
-        </div>
-      </aside>
 
+      {/* section com img, aside e btn que aparecerá somente no mobile */}
+      <FirstSection />
 
       <section className={clsx('relative flex justify-center items-center')}>
         {/* <Image className={clsx('w-full')} src={imgCasal} alt="" />
@@ -50,7 +26,6 @@ export default function Home() {
           <h1 className={clsx('text-white font-beau text-8xl')}>Alessandra & Luiz</h1>
           <p className={clsx('text-white font-cinzel text-2xl')}>29 de março de 2025</p>
         </div> */}
-
 
       </section>
     </main>
---
+++ components/sections_v2/FirstSection.tsx
@@ -0,0 +1,64 @@
+//--Imports
+import Image from "next/image";
+import { useState } from "react";
+
+//--Libs
+import { ArrowLeftFromLineIcon, AlignJustify } from 'lucide-react'
+import clsx from "clsx";
+
+//--Assets
+import imgCasal from '@/public/img/img-casal-example.jpg'
+import luizele from '@/public/img/img-casal.png'
+
+export default function FirstSection() {
+
+    const [showAside, setShowAside] = useState(true)
+
+    return (
+        <section className="relative w-full flex flex-col md:hidden h-screen">
+            {/* bg casal */}
+            <div className="absolute inset-0 w-full h-3/6">
+                <Image
+                    src={imgCasal}
+                    alt=""
+                    layout="fill"
+                    objectFit="cover"
+                    className="z-0"
+                />
+            </div>
+
+            {/* aside sobreposto a img */}
+            <div className="relative z-10 flex justify-between">
+                <aside className={clsx(
+                    'transition-all ease-in-out duration-500 h-screen font-abhaya font-semibold text-Cinza_main_100 text-xl',
+                    'overflow-hidden w-64 bg-white border-r shadow-sm z-20',
+                    !showAside && 'w-0 opacity-0 translate-x-[-100%]'
+                )}>
+                    <div className="h-full flex flex-col">
+                        <div className="h-auto flex flex-col bg-[#f2fcfc] py-6 px-8 gap-2">
+                            <div onClick={() => setShowAside(false)} className="self-end cursor-pointer">
+                                <ArrowLeftFromLineIcon />
+                            </div>
+                            <Image className="w-5/12" src={luizele} alt="" />
+                            <p className="text-lg">Alessandra <span className="text-red-400">&</span> Luiz Lima</p>
+                        </div>
+                        <div className="w-auto h-full flex flex-col py-6 px-8 gap-8">
+                            <p className="cursor-pointer">Home</p>
+                            <p className="cursor-pointer">O Casal</p>
+                            <p className="cursor-pointer">Cerimônia</p>
+                            <p className="cursor-pointer">Presentes</p>
+                            <p className="cursor-pointer">Recados</p>
+                        </div>
+                    </div>
+                </aside>
+
+                {/* btn alignjustify */}
+                <div className="p-2 relative z-20">
+                    <div className="p-2 rounded h-fit cursor-pointer bg-[#40495195]" onClick={() => setShowAside(!showAside)}>
+                        <AlignJustify color="white" size={32} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
